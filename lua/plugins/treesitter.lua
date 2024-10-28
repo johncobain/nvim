dd53b8fac78742c9dd4b3f740064ac300dd813f0
John <andreygdsn@gmail.com>
2024-10-28 18:01:03 -0300

    neoVim config

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,13 @@
+return{
+	"nvim-treesitter/nvim-treesitter", 
+	build = ":TSUpdate",
+	config = function()
+		require ('nvim-treesitter.install').compilers = { 'zig' }
+		local config = require("nvim-treesitter.configs") 
+		config.setup({
+		ensure_installed = { "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+		highlight = { enable = true },
+        	indent = { enable = true },
+		})
+	end
+}
