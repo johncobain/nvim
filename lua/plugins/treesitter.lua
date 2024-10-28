a6e19e784b43c4cdd5b204ee8d04d494c6533f0d
John <andreygdsn@gmail.com>
2024-10-28 19:03:42 -0300

    mason lsp config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return{
 		require ('nvim-treesitter.install').compilers = { 'zig' }
 		local config = require("nvim-treesitter.configs") 
 		config.setup({
-		ensure_installed = { "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+		ensure_installed = { "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "css", "python", "c", "cpp" },
 		highlight = { enable = true },
         	indent = { enable = true },
 		})
