8016ca08b48129e6b5871b2d0ebe9f1a2f3d0e5f
Andrey <andreygdsn@gmail.com>
2025-05-15 22:43:51 -0300

    fix: treesitter"

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,13 +1,17 @@
-return{
-	"nvim-treesitter/nvim-treesitter", 
-	build = ":TSUpdate",
-	config = function()
-		require ('nvim-treesitter.install').compilers = { 'zig' }
-		local config = require("nvim-treesitter.configs") 
-		config.setup({
-		auto_install = true,
-		highlight = { enable = true },
-        	indent = { enable = true },
-		})
-	end
+return {
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",      -- compila/atualiza parsers no :Lazy sync
+  event = { "BufReadPost", "BufNewFile" }, -- só carrega quando abrir arquivo
+  opts = {
+    ensure_installed = { "lua", "vim", "vimdoc", "bash", "json", "markdown" },
+    auto_install     = false,   -- não tenta baixar parser faltando na hora
+    sync_install     = false,
+    highlight        = { enable = true },
+    indent           = { enable = true },
+  },
+  config = function(_, opts)
+    -- diz explicitamente quais compiladores tentar – na ordem
+    require("nvim-treesitter.install").compilers = { "gcc", "clang", "zig" }
+    require("nvim-treesitter.configs").setup(opts)
+  end,
 }
