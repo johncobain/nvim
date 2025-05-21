42d835e9b7c13e13734903a693f24d43c9182555
Andrey <andreygdsn@gmail.com>
2025-05-21 16:23:25 -0300

    use terminal (C-t for toggle C-j for normal mode)

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -6,6 +6,28 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 vim.g.mapleader = " "
 
+-- Sair do modo terminal com 'jj'
+vim.keymap.set("t", "<C-j>", [[<C-\><C-n>]], { desc = "Sair do terminal (modo normal)" })
+
+-- Toggle terminal
+function _G.toggle_terminal()
+  for _, win in ipairs(vim.api.nvim_list_wins()) do
+    local buf = vim.api.nvim_win_get_buf(win)
+    local buftype = vim.api.nvim_buf_get_option(buf, "buftype")
+    if buftype == "terminal" then
+      vim.api.nvim_win_close(win, true)
+      return
+    end
+  end
+  vim.cmd("botright split | terminal")
+  vim.cmd("startinsert")
+end
+
+vim.keymap.set("t", "<C-t>", toggle_terminal, { desc = "Sair do terminal (modo normal)" })
+
+
+vim.keymap.set("n", "<C-t>", toggle_terminal, { desc = "Toggle Terminal" })
+
 vim.api.nvim_create_augroup("neotree", {})
 vim.api.nvim_create_autocmd("UiEnter", {
 	desc = "Open Neotree automatically",
@@ -16,3 +38,4 @@ vim.api.nvim_create_autocmd("UiEnter", {
 		end
 	end,
 })
+
